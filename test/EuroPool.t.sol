c9736edaf0425b2d0b7ea7337e0a7e8d199b740e
DavNej <david.nejar@gmail.com>
2024-02-17 12:23:17 +0100

    EuroPoolTest inherits from HelperEuroPool

--- test/EuroPool.t.sol
+++ test/EuroPool.t.sol
@@ -4,62 +4,43 @@ pragma solidity ^0.8.20;
 import {Test, console} from "forge-std/Test.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-
 import {DeployEuroPool} from "../script/DeployEuroPool.s.sol";
-import {MockERC20} from "./mock/MockERC20.sol";
-
-import {EuroPool} from "../src/EuroPool.sol";
+import {HelperEuroPool} from "./HelperEuroPool.t.sol";
+import {MockERC20} from "./MockERC20.sol";
 
 // Simple test suite that tests the EuroPool contract. This suite does not include gas price for simplicity reasons
 
-contract EuroPoolTest is Test {
-    IERC20 token;
-    EuroPool euroPool;
-
-    address euroPoolOwner;
-    address tokenDeployer;
-
-    address ALICE = makeAddr("alice");
-    address BOB = makeAddr("bob");
-    address CHARLES = makeAddr("charles");
-
+contract EuroPoolTest is HelperEuroPool {
     function setUp() external {
         DeployEuroPool deployEuroPool = new DeployEuroPool();
-        euroPool = deployEuroPool.run();
-        token = IERC20(deployEuroPool.tokenAddress());
-        tokenDeployer = msg.sender;
-        euroPoolOwner = euroPool.owner();
+        s_euroPool = deployEuroPool.run();
+        s_token = IERC20(deployEuroPool.tokenAddress());
+        s_tokenDeployer = msg.sender;
 
-        vm.startPrank(tokenDeployer);
-        token.transfer(euroPoolOwner, 10000 ether);
-        token.transfer(ALICE, 100 ether);
-        token.transfer(BOB, 100 ether);
-        token.transfer(CHARLES, 100 ether);
-        vm.stopPrank();
-
-        console.log("tokenDeployer balance", token.balanceOf(msg.sender));
-        console.log("ALICE balance", token.balanceOf(ALICE));
-        console.log("BOB balance", token.balanceOf(BOB));
-        console.log("CHARLES balance", token.balanceOf(CHARLES));
+        fundUsersWithToken();
+        fundRewardPool();
     }
 
     function testOwnerIsThisContract() public {
-        assertEq(euroPool.owner(), address(this));
+        assertEq(s_euroPool.owner(), address(this));
     }
 
     function testOwnerCanFundRewardPool() public {
-        vm.startPrank(euroPoolOwner);
-        token.approve(address(euroPool), 100 ether);
-        euroPool.fundRewardPool(100 ether);
+        uint256 amountToFund = 100 ether;
+
+        vm.startPrank(s_euroPool.owner());
+        s_token.approve(address(s_euroPool), amountToFund);
+        s_euroPool.fundRewardPool(amountToFund);
         vm.stopPrank();
 
-        assertEq(token.balanceOf(address(euroPool)), 100 ether);
+        assertEq(s_token.balanceOf(address(s_euroPool)), INITIAL_REWARD_POOL_SIZE + amountToFund);
     }
 
+    // Ensures that only the contract owner can fund the reward pool, and others are reverted.
     function testOnlyOwnerCanFundRewardPool() public {
         vm.startPrank(ALICE);
         vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, ALICE));
-        euroPool.fundRewardPool(1 ether);
+        s_euroPool.fundRewardPool(1 ether);
         vm.stopPrank();
     }
-}
+
